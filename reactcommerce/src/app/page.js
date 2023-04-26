43a39108d1c2953fd5f25eed6cf53099ba0cc7a0
Jerry <jamesharold1738@gmail.com>
2023-04-26 00:19:50 -0400

    added next.js and yarn and tailwindcss

---
+++ reactcommerce/src/app/page.js
@@ -0,0 +1,12 @@
+//import Image from 'next/image'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className ={'bg-red-900 w-screen h-screen flex items-center'}>
+      <div className={'text-center w-full'}>
+<button className = {'bg-white p-2 rounded-lg px-4'}>login with Google</button>
+</div>
+    </div>
+  )
+}
